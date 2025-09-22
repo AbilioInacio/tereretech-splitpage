6eb27321a566116abc7958b44a5f568ab0e05ada
Abílio Inácio <abilio.torres.03@gmail.com>
2025-09-22 03:37:02 +0000

    Correçoes dos botões fixos e da secao final

--- pag_vendas/nano/script.js
+++ pag_vendas/nano/script.js
@@ -30,4 +30,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Adicione mais funcionalidades JavaScript aqui, se necessário.
   // Ex: Animações ao rolar, validação de formulário (se houver), etc.
+  //botoes somem na secao buy
+  // selecionando botão "Comprar Agora"
+  const botaoComprar = document.getElementById("btn-comprar-fixo");
+
+  // Selecionando botão do WhatsApp
+  const botaoWhats = document.getElementById("btn-whatsapp-fixo");
+
+  // Detectar quando a última seção está visível
+  const ultimaSecao = document.getElementById("buy");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Oculta os botões
+          botaoComprar.style.opacity = 0;
+          botaoWhats.style.opacity = 0;
+        } else {
+          // Mostra os botões
+          botaoComprar.style.opacity = 1;
+          botaoWhats.style.opacity = 1;
+        }
+      });
+    },
+    {
+      threshold: 0.5, // ativa quando 50% da seção estiver visível
+    }
+  );
+
+  observer.observe(ultimaSecao);
 });
